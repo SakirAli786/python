903c434113a3c5602feb64b2f09c10d4000adeca
Sakir Ali <118117387+SakirAli786@users.noreply.github.com>
2023-05-27 22:41:12 +0530

    Add files via upload

--- first.py
+++ first.py
@@ -1,1 +1,18 @@
+# name= 'Sakir'
+# age=23
+# dist='gopalganj'
+# print( name,' is a boy')
+# print( name,' is', +age)
+# print( name,'is from' ,dist)
 
+
+# print(name) 
+# print('hi.\nHow are you')
+# print('Hi \'How are you')
+
+name="sakir"
+# print(name.replace('i', 'a'))
+# print(name.index('i'))
+# print(len(name))
+print(name[0])
+ 
